1b3fc3363c3c54ea0dd0aaf96a438b294789d2ab
Drew <drew@dhornbein.com>
2022-01-09 15:48:55 -0700

    nav transition

--- components/nav-bar.vue
+++ components/nav-bar.vue
@@ -1,44 +1,51 @@
 <template>
-  <nav class="nav" :class="{'is-open': mobileMenuOpen}">
-      
-    <nuxt-link to="/" class="md:order-2 flex items-center">
-        <img src="/img/web-logo-circle-color-shape.svg" class="max-h-10">
-        <span class="text-xl text-white font-bold ml-2" :class="{ hidden: !mobileMenuOpen }">People Medicine</span>
-    </nuxt-link>
-      
-    <ul class="" 
-      v-for="(group, name, index) in navigation" :key="index" 
-      :class="[group.class, { hidden: !group.showMobile, flex: group.showMobile }]"
-    >
-      <router-link v-for="item in group.links" :key="item.url"
-        :to="item.url"
-        v-slot="{ href, navigate, isActive, isExactActive }"
-        custom
-      >
-        <li class=""
-          :class="{'link-active': isActive, 'link-exact-active': isExactActive}"
-        >
-          <a class=""
-            :href="href" @click="navigate">{{ item.name }}</a>
-        </li>
-      </router-link>
-    </ul>
-    <div class="burger">
-      <button :class="{ opened: mobileMenuOpen }" @click="mobileMenuOpen = !mobileMenuOpen" aria-label="Main Menu" :aria-expanded="mobileMenuOpen">
-        <svg width="100" height="100" viewBox="0 0 100 100">
-          <path class="line line1" d="M 20,29.000046 H 80.000231 C 80.000231,29.000046 94.498839,28.817352 94.532987,66.711331 94.543142,77.980673 90.966081,81.670246 85.259173,81.668997 79.552261,81.667751 75.000211,74.999942 75.000211,74.999942 L 25.000021,25.000058" />
-          <path class="line line2" d="M 20,50 H 80" />
-          <path class="line line3" d="M 20,70.999954 H 80.000231 C 80.000231,70.999954 94.498839,71.182648 94.532987,33.288669 94.543142,22.019327 90.966081,18.329754 85.259173,18.331003 79.552261,18.332249 75.000211,25.000058 75.000211,25.000058 L 25.000021,74.999942" />
-        </svg>
-      </button>
-    </div>
+  <div class="nav-wrapper">
+    <nav class="nav" :class="{'is-open': mobileMenuOpen}">
     
-  </nav>
+      <nuxt-link to="/" class="md:order-2 flex items-center">
+          <img src="/img/web-logo-circle-color-shape.svg" class="max-h-10">
+          <span class="text-xl text-white font-bold ml-2" :class="{ hidden: !mobileMenuOpen }">People Medicine</span>
+      </nuxt-link>
+    
+      <ul class=""
+        v-for="(group, name, index) in navigation" :key="index"
+        :class="[group.class, { hidden: !group.showMobile, flex: group.showMobile }]"
+      >
+        <router-link v-for="item in group.links" :key="item.url"
+          :to="item.url"
+          v-slot="{ href, navigate, isActive, isExactActive }"
+          custom
+        >
+          <li class=""
+            :class="{'link-active': isActive, 'link-exact-active': isExactActive}"
+          >
+            <a class=""
+              :href="href" @click="navigate">{{ item.name }}</a>
+          </li>
+        </router-link>
+      </ul>
+      <div class="burger">
+        <button :class="{ opened: mobileMenuOpen }" @click="mobileMenuOpen = !mobileMenuOpen" aria-label="Main Menu" :aria-expanded="mobileMenuOpen">
+          <svg width="100" height="100" viewBox="0 0 100 100">
+            <path class="line line1" d="M 20,29.000046 H 80.000231 C 80.000231,29.000046 94.498839,28.817352 94.532987,66.711331 94.543142,77.980673 90.966081,81.670246 85.259173,81.668997 79.552261,81.667751 75.000211,74.999942 75.000211,74.999942 L 25.000021,25.000058" />
+            <path class="line line2" d="M 20,50 H 80" />
+            <path class="line line3" d="M 20,70.999954 H 80.000231 C 80.000231,70.999954 94.498839,71.182648 94.532987,33.288669 94.543142,22.019327 90.966081,18.329754 85.259173,18.331003 79.552261,18.332249 75.000211,25.000058 75.000211,25.000058 L 25.000021,74.999942" />
+          </svg>
+        </button>
+      </div>
+    
+    </nav>
+  </div>
 </template>
 
 <script>
 export default {
   props: {
+  },
+  watch:{
+    $route (to, from){
+        this.mobileMenuOpen = false;
+    }
   },
   data() {
     return {
@@ -98,8 +105,11 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.nav-wrapper {
+  @apply h-14;
+}
 nav {
-  @apply bg-dark text-brand-cream w-full flex p-2 relative;
+  @apply bg-dark text-brand-cream w-full flex p-2 relative transition-all;
 
   ul {
     li {
@@ -116,7 +126,7 @@ nav {
   }
   
   &:not(.is-open) { // default navigation
-    @apply max-h-14 items-center justify-between lg:justify-center;
+    @apply h-14 items-center justify-between lg:justify-center;
     ul {
       @apply sm:justify-items-start md:justify-around flex-grow md:flex md:w-5/12 lg:flex-shrink;
       li {
