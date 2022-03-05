ff95fbae40d1d60cc928c52a1a1d4cda719a18be
Drew <git@dhornbein.com>
2022-03-04 20:47:18 -0700

    adds case study draft

---
+++ pages/case-study/gitcoin-retreat.vue
@@ -0,0 +1,220 @@
+<template>
+  <article>
+    <header class="header p-8 mb-8 bg-white">
+      <div class="content flex gap-8 items-center">
+        <figure class="max-w-sm inline-block">
+          <img src="~/assets/img/gitcoin-logo.svg" alt="GitCoin">
+        </figure>
+        <div>
+          <h1 class="text-4xl">2022 Retreat Sessions</h1>
+          <p class="max-w-prose text-lg m-auto">
+            In early 2022 we hosted two 4 hour sessions for GitCoin, a community of builders, creators, and protocols at the center of open web ecosystems.
+          </p>
+        </div>
+      </div>
+    </header>
+
+    <section class="">
+      <div class="content flex gap-8 items-center">
+        <div class="email max-w-md m-2 p-4 bg-white shadow-md rounded-md font-mono">
+          <header class="bg-gray-800 text-white px-4 py-2 -mx-4 -mt-4 rounded-t-md mb-2 flex items-center justify-between">
+            <span>subject: team training</span>
+            <svg class="w-6 h-6 text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"></path><path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"></path></svg>
+          </header>
+          <div class="text-gray-500 mb-2">to: People Medicine</div>
+          <blockquote>
+            Hello I'm Loie, and I lead People Operations in Gitcoin.
+            I'm planning a team retreat for our company &amp; our DAO to join together &amp; spend quality time, some of which includes (we hope!)
+            training on how we can work better together, perhaps from someone like yourself.
+          </blockquote>
+        </div>
+        <div>
+          <h2 class="text-2xl">Finding Fit</h2>
+          <p>
+            We start these kinds of engagements with conversations and relationship building.
+            Picking up on vibes to sense good fit. Even over a screen our bodies and minds can sense certain alignment.
+          </p>
+        </div>
+      </div>
+    </section>
+
+    <section class="">
+      <div class="content flex gap-8 items-center">
+        <div>
+          <h2 class="text-2xl">Scope of Work</h2>
+          <p>
+            Based on our initial conversations we deliver and discuss a first draft of a Scope of Work.
+            Reflecting back our understanding of the request from Loie and GitCoin.
+          </p>
+          <p>
+            Based on feedback from the draft we better align our visions until we get this feedback:
+          </p>
+        </div>
+        <div class="p-8 bg-dark text-brand-cyan rounded-md relative">
+          <div class="text-lg text-brand-cyan absolute top-2 right-2">
+            <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M9.983 3v7.391c0 5.704-3.731 9.57-8.983 10.609l-.995-2.151c2.432-.917 3.995-3.638 3.995-5.849h-4v-10h9.983zm14.017 0v7.391c0 5.704-3.748 9.571-9 10.609l-.996-2.151c2.433-.917 3.996-3.638 3.996-5.849h-3.983v-10h9.983z"/></svg>
+          </div>
+          <blockquote class="text-lg font-serif italic border-l-2 border-brand-cyan pl-4">
+            I would say you understand the problem space FANTASTICALLY. And i like what i hear so far of the activities to navigate it. Reading this I super trust your vision for session 2!
+          </blockquote>
+        </div>
+      </div>
+    </section>
+
+    <section class="">
+      <div class="flex gap-8 p-8 items-stretch bg-black shadow-md relative rounded-b-md max-w-screen-lg mx-auto">
+        <div class="bar absolute px-4 -top-8 left-0 w-full h-8 bg-gray-800 rounded-t-md flex items-center justify-between">
+          <div class="flex gap-2">
+            <div class="bg-red-600 w-3 h-3 rounded-full"></div>
+            <div class="bg-yellow-600 w-3 h-3 rounded-full"></div>
+            <div class="bg-green-600 w-3 h-3 rounded-full"></div>
+          </div>
+          <div class="text-gray-400">Online Session</div>
+          <div>
+            <svg class="w-6 h-6 text-blue-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M2 6a2 2 0 012-2h6a2 2 0 012 2v8a2 2 0 01-2 2H4a2 2 0 01-2-2V6zM14.553 7.106A1 1 0 0014 8v4a1 1 0 00.553.894l2 1A1 1 0 0018 13V7a1 1 0 00-1.447-.894l-2 1z"></path></svg>
+          </div>
+        </div>
+        <div class="zoom text-white bg-opacity-20 flex-grow flex justify-center items-center">
+          <div class="max-w-prose">
+            <h2 class="text-2xl">Appreciative Inquiry</h2>
+            <p>
+              We accept an invitation to join GitCoin's low key Friday intra team meeting where they spend time sharing ideas.
+              We bring a short offering overview of Appreciative Inquiry. We ask why they like working at GitCoin.
+            </p>
+          </div>
+        </div>
+        <div class="chat max-w-sm">
+          <header>
+            From session chat
+          </header>
+          <div class="msg">
+            <div class="meta">From <strong class="text-brand-pink">People Medicine</strong> to <span>Everyone</span>:</div>
+            <p>🧭 Discover - what are our strengths?</p>
+          </div>
+          <div class="msg">
+            <div class="meta">From GitCoiner to <span>Everyone</span>:</div>
+            <p>I get to work with kind, super smart people who want to make the world a better place</p>
+          </div>
+          <div class="msg">
+            <div class="meta">From GitCoiner to <span>Everyone</span>:</div>
+            <p>a safe place to be ones self</p>
+          </div>
+          <div class="msg">
+            <div class="meta">From GitCoiner to <span>Everyone</span>:</div>
+            <p>I get to work on a product that directly serves and provides value to a community I care deeply about!</p>
+          </div>
+          <div class="msg">
+            <div class="meta">From <strong class="text-brand-pink">People Medicine</strong> to <span>Everyone</span>:</div>
+            <p>💭 Dream - how do we envision ourselves ___ years from now?</p>
+          </div>
+          <div class="msg">
+            <div class="meta">From GitCoiner to <span>Everyone</span>:</div>
+            <p>We will be wildly successful when 10000s of developers can quit their job + work on the open internet</p>
+          </div>
+          <div class="msg">
+            <div class="meta">From GitCoiner to <span>Everyone</span>:</div>
+            <p>Gitcoin will be the leading voice in shifting how people think about work</p>
+          </div>
+        </div>
+      </div>
+
+    </section>
+
+    <section class="">
+      <div class="content flex gap-8 items-center">
+        <div>
+          <nuxt-link to="/resources/6-tools-for-group-communication">
+            <figure class="paper-stack w-80">
+              <img src="https://drive.google.com/thumbnail?authuser=0&sz=w320&id=120NxBW0KQYpazmeryz0jocQQsPKKHw6eLTjw-szx42g" alt="6 Skills For Group Communication">
+            </figure>
+          </nuxt-link>
+        </div>
+        <div>
+          <h2 class="text-2xl">Session 1: Making Feedback Regenerative</h2>
+          <p>
+            In this session we move through the 6 Living System Norms for Group Communication.
+            Introducing each concept then inviting participants to recreate problems from their past
+            that could have been made less troublesome with these simple communication skills.
+            We notice that past work related wounds are brought up and kinship is found around
+            our shared challenges with past work communication dynamics.
+          </p>
+        </div>
+      </div>
+    </section>
+
+    <section class="">
+      <div class="content flex gap-8 items-center">
+        <div>
+          <h2 class="text-2xl">Session 2: The 3 Levels of Coordination</h2>
+          <p>
+            On the second day we adjusted our plans based on the feedback we received from
+            watching the team work and play on day 1. We turned towards a meme within their organization
+            around “the coordination problem” and framed our thinking on this problem using 3 levels of a pyramid.
+            Each level describes “setting up the work”, “doing the work”, and “growing the work” in turn.
+            We facilitated discussions around each level, pulling together the group intelligence in the
+            room to apply our framing to GitCoin. From these inquiries emerged discussion subjects which
+            we coordinated using an adaptation of Open Space Technology.
+            Each group worked on their problem then reported back to the full group on the “highest-impact insight”.
+          </p>
+        </div>
+        <div>
+          <nuxt-link to="/resources/3-levels-of-coordination">
+            <figure class="paper-stack w-80">
+              <img src="https://drive.google.com/thumbnail?authuser=0&sz=w320&id=1iJaa2S-YtuWcUodz-OGM1L4R-jplKPVo4MuL4-4GHIg" alt="The 3 Levels Of Coordination">
+            </figure>
+          </nuxt-link>
+        </div>
+      </div>
+    </section>
+
+    <section>
+      <div class="container max-w-prose mx-auto">
+        <h2 class="text-2xl">Thoughts and Reflections</h2>
+        <p>This was our first in-person facilitation job as People Medicine in almost 2 years. It was the first in-person gathering many of these GitCoin members had ever had. The energy was quite powerful in the room. Furthermore, this group of people seemed to be very present and accepting of our facilitation, games, and exercises. They came with a “yes and” attitude. They were critical and helpful, curious and wise. A thrill to work with. Only time will tell what kind of impact our sessions will have had. Though in the brief afterglow of the space we hosted here is an sampling of the feedback we received:</p>
+
+        <div class="p-8 mt-8 bg-dark text-brand-pink rounded-md relative">
+          <div class="text-lg text-brand-pink absolute top-2 right-2">
+            <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M9.983 3v7.391c0 5.704-3.731 9.57-8.983 10.609l-.995-2.151c2.432-.917 3.995-3.638 3.995-5.849h-4v-10h9.983zm14.017 0v7.391c0 5.704-3.748 9.571-9 10.609l-.996-2.151c2.433-.917 3.996-3.638 3.996-5.849h-3.983v-10h9.983z"/></svg>
+          </div>
+          <blockquote class="text-lg font-serif italic border-l-2 border-brand-pink pl-4">
+           It was great! I don't usually speak up because I prefer to listen but I managed to find the courage to talk because of the space and environment you've created that made me comfortable.
+          </blockquote>
+        </div>
+      </div>
+    </section>
+
+
+  </article>
+</template>
+
+<style lang="scss" scoped>
+  .content {
+    @apply max-w-screen-lg mx-auto;
+  }
+  section {
+    @apply my-20 p-8
+  }
+  .zoom {
+    background: 
+    linear-gradient(
+      rgba(0, 0, 0, .9), 
+      rgba(0, 0, 0, 0.6)
+    ),
+    #000 url('~assets/img/zoom-gallery-view.png') no-repeat center/cover;
+  }
+  .chat {
+    @apply p-4 -my-8 -mr-8 bg-white border-l border-gray-400;
+    header {
+      @apply -mx-4 -mt-2 mb-2 pb-2 border-b border-gray-400 text-center text-lg font-bold text-gray-600;
+    }
+    .msg {
+      @apply font-mono text-sm my-2;
+      .meta {
+        @apply text-gray-400;
+        span {
+          @apply text-blue-400;
+        }
+      }
+    }
+  }
+</style>
---
+++ pages/case-study/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1 class="text-4xl text-center p-8">Case Studies</h1>
+
+    <div class="p-4 max-w-sm">
+      <nuxt-link to="/case-study/gitcoin-retreat">
+        <img src="~/assets/img/gitcoin-logo.svg" alt="GitCoin">
+      </nuxt-link>
+    </div>
+  </div>
+</template>
